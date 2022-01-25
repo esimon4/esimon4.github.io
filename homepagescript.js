5fdb62f48ebf716da2a8fc63d7fcdcb1f905fb58
esimon4 <ersimon@usc.edu>
2022-01-25 15:17:34 +0100

    newhoneybis

--- homepagescript.js
+++ homepagescript.js
@@ -1,4 +1,11 @@
+//setting up mail client trigger
+
 document.querySelector("#contactbutton").addEventListener("click", function(){
     // alert("hey!");
     window.location = "mailto:ersimon@usc.edu";
 });
+
+//
+document.querySelector("#honeybis").addEventListener("click", function(){
+    window.open("honeybis.html", "_self");
+})
